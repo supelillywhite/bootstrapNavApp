7d47192c4fe8910e2584b68b188057250422e859
Jerel Lillywhite <supelillywhite@Jerels-MacBook-Pro.local>
2017-12-13 21:01:55 -0700

    Basic bootstrap implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
   get 'pages/about'
 
@@ -9,5 +8,5 @@ Rails.application.routes.draw do
 
   get 'pages/future'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
